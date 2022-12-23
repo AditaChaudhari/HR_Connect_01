44ecf2168d8a61488503e562da1f59be279189f8
AditaChaudhari <aditachaudhari07@gmail.com>
2022-12-24 00:46:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# CLI application to perform tasks on csv file of employee details
+from taskone import do_task_one
+from tasktwo import do_task_two
+
+if __name__ == '__main__':
+    do_task_one()
+    do_task_two()
